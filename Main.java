081b61440c600c30857cd331399911c94c164b66
Antonino <pagana899@gmail.com>
2024-01-15 23:40:38 +0100

    Esercizio Completo 2

--- Main.java
+++ Main.java
@@ -5,7 +5,15 @@ import java.util.Locale;
 public class Main {
     public static void main(String[] args) {
         OffsetDateTime data = OffsetDateTime.parse("2023-03-01T13:00:00Z");
-        String dataFormattata = data.format(DateTimeFormatter.ofPattern("dd MMMM yyyy"));
-        System.out.println(dataFormattata);
+
+    }
+
+    public String formattaData(OffsetDateTime data) throws Exception {
+        if (data != null) {
+            String dataFormattata = data.format(DateTimeFormatter.ofPattern("dd MMMM yyyy"));
+            return dataFormattata;
+        } else {
+            throw new Exception();
+        }
     }
 }
--- MainTest.java
+++ MainTest.java
@@ -6,12 +6,17 @@ import java.time.format.DateTimeFormatter;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MainTest {
+    Main testing = new Main();
 
     @Test
     void controlloStringaFormattata() {
-        OffsetDateTime data = OffsetDateTime.parse("2023-03-01T13:00:00Z");
-        String actual = data.format(DateTimeFormatter.ofPattern("dd MMMM yyyy"));
-        String expected = "01 marzo 2023";
-        assertEquals(expected, actual);
+        try {
+            OffsetDateTime data = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+            String result = testing.formattaData(data);
+            assertEquals("01 marzo 2023", result);
+        } catch (Exception e) {
+            System.out.println("Eccezione: " + e.getMessage());
+        }
+
     }
 }
